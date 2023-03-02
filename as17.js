54d437e91a5e147b26487dab620786c35bc3a7b3
DigitalSubham <98773948+DigitalSubham@users.noreply.github.com>
2023-03-03 02:14:22 +0530

    Add files via upload

---
+++ as17.js
@@ -0,0 +1,60 @@
+// 1. Create a function to iterate over the following list [“John”,
+// “Rohn”, “Danny”, “James”]
+
+// function iterate(){
+//     arr = ["John","Rohn", "Danny", "James"]
+//     for(let i of arr){
+//         console.log(i);
+//     }
+// }
+// iterate()
+
+
+// 2. Iterate over all the characters of the word “iNeuron”.
+
+// function iterate1(){
+//     str =  "iNeuron"
+//     for(let i of str){
+//         console.log(i);
+//     }
+// }
+// iterate1()
+
+
+// 3. Create a functional iterator, with a next function.
+
+// not understand question
+
+// 4. In the following two arrays find which two elements match
+// using iterators.
+// Array 1 - [“a”, “b”, “c”, “d”]
+// Array 2 - [“e”, “f”, “g”, “h”, “a”, “i”, “j”]
+
+// const array1 = ["a", "b", "c", "d"];
+// const array2 = ["e", "f", "g", "h", "a", "i", "j"];
+
+// for (const element1 of array1) {
+//   for (const element2 of array2) {
+//     if (element1 === element2) {
+//       console.log(`Match found: ${element1}`);
+//     }
+//   }
+// }
+
+
+
+
+
+// 5. What are the different ways of iterating through an array?
+// demonstrate them.
+
+
+//  some common methods for iterating through an array in JavaScript:
+
+// for loop
+// forEach() method
+// map() method
+// filter() method
+// reduce() method
+// for-of loop
+// for-in loop
